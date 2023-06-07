d0cda490d0fb6f95f20ac2e2a7cc2c62dec2e0d0
Andriievskyi <andriyevski@gmail.com>
2023-06-07 19:10:07 +0300

    Add and use "unittest", and change name of functional tests

--- function_tests.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser_link=browser.get('Http://localhost:8000')
-
-
-assert 'The install worked successfully! Congratulations!' in browser.title
---
+++ functional_tests.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    ''' Test new visitors '''
+
+    def setUp(self):
+        ''' Install '''
+        # Chrome more faster then anather in tests
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        ''' Dismantling '''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_leter(self):
+        ''' Test: can start list and take him later '''
+
+        # Go to site
+        self.browser.get('http://localhost:8000')
+
+        # Watch the title of page
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End the test!')
+
+        # Add the element in list
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
