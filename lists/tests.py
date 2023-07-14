2d01b62c24dce3c065b29e2996060b5eb3c15984
andriyevski <andriyevski@gmail.com>
2023-07-15 01:05:18 +0300

    First modul test and transform url, fictitious representation

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    ''' Toxic test for check if works test '''
+class HomePageTest(TestCase):
+    ''' Toxic test for check home page '''
 
-    def test_bad_maths(self):
-        ''' Test: Wrong maths result '''
-        self.assertEquals(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        ''' Test: root url change to view of home_page '''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
